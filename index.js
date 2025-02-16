f350febfa209cb92ccacd9b1ca91b8827abdf765
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-02-16 15:04:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// let n=0;
+// if (n < 0) {
+// 	console.log("Number is negative");
+// } else if (n % 2 === 0) {
+// 	console.log("Even");
+// } else {
+// 	console.log("Odd");
+// }
+
+// //
+// const sum = arr.reduce((acc, num) => acc + num, 0); 
+// console.log(sum);
+
+// //
+// const reversedString = s.split('').reverse().join(''); 
+// console.log(reversedString);
+
+
+// //
+// const maxElement = Math.max(...arr);
+// console.log(maxElement);
+
+// //
+// const reversedString = s.split('').reverse().join('');
+
+// if (s === reversedString) {
+// 	console.log("Yes");
+// } else {
+// 	console.log("No");
+// }
+
+
+//-----------------------
+console.log('Abdul Mazid Akash');
