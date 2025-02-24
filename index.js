baa45829ffd50c8c02473f0f0437af38fce5b46d
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-02-24 23:36:07 +0600

    commit

--- index.js
+++ index.js
@@ -31,4 +31,62 @@
 
 
 //-----------------------
-console.log('Abdul Mazid Akash');
+console.log('<--SCIC-BATCH-10-->');
+
+function inchToFeet(inch){
+	const feet = inch / 12;
+	return feet ;
+}
+
+// console.log(inchToFeet(75));
+
+function inchToFeet2(inch){
+	const feetFraction = inch / 12 ;
+	const feetNumber = parseInt(feetFraction);
+	const inchRemaining = inch % 12 ;
+	const result = feetNumber + ' feet ' + inchRemaining + ' inch';
+	return result ;
+}
+
+// console.log(inchToFeet2(75));
+
+// console.log(inchToFeet2);
+
+function mileToKilometer(mile){
+	const kilo = mile * 1.60934;
+	return kilo;
+}
+
+// console.log(mileToKilometer(20));
+
+function kiloMeterToMiles(kilo){
+	const mile = kilo * 0.621371;
+	return mile ;
+}
+
+// console.log(kiloMeterToMiles(75));
+
+function isLeapYear(year){
+	if(year % 4 === 0){
+		return `${year} is leap year`;
+	}else{
+		return `${year} is not leap year`;
+	}
+}
+
+// console.log(isLeapYear(2052));
+// console.log(isLeapYear(2053));
+
+function isLeapYear2(year){
+	if(year % 100 !== 0 && year % 4 === 0){
+		return `${year} is leap year`
+	}else if(year % 400 === 0){
+		return `${year} is leap year.condition 400 divide.`;
+	}else{
+		return `${year} is not leap year`
+	}
+}
+
+console.log(isLeapYear2(2052));
+console.log(isLeapYear2(2400));
+console.log(isLeapYear2(2100));
